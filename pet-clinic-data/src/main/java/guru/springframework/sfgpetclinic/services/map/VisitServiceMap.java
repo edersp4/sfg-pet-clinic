aadccdb492ccd52f3c37a24cf8a506e878655139
edersondasilva <edsilvasp@hotmail.com>
2019-01-19 12:05:53 -0200

    Create Visit Map Service closes #46

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/VisitServiceMap.java
@@ -0,0 +1,49 @@
+package guru.springframework.sfgpetclinic.services.map;
+
+import guru.springframework.sfgpetclinic.model.Visit;
+import guru.springframework.sfgpetclinic.services.SpecialityService;
+import guru.springframework.sfgpetclinic.services.VisitService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class VisitServiceMap extends AbstractMapService<Visit, Long> implements VisitService {
+
+    private final SpecialityService specialityService;
+
+    public VisitServiceMap(SpecialityService specialityService) {
+        this.specialityService = specialityService;
+    }
+
+
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+        if (object.getPet() == null || object.getPet().getOwner() == null || object.getPet().getId() == null
+        || object.getPet().getOwner().getId() == null) {
+            throw new RuntimeException("Invalid Visit");
+        }
+
+        return super.save(object);
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+
+}
