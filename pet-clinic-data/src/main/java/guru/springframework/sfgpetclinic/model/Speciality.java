70f53fdb272940d362585a7f4290627fecf24cf2
edersondasilva <edsilvasp@hotmail.com>
2019-01-19 10:14:17 -0200

    update vets with Jpa entity mappings closes #36

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Speciality.java
@@ -2,6 +2,11 @@ package guru.springframework.sfgpetclinic.model;
 
 import guru.springframework.sfgpetclinic.services.BaseEntity;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "specialities")
 public class Speciality extends BaseEntity {
     private String description;
 
