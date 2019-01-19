93b9a1ad294537466edcfa7972e1b2956b195dd8
edersondasilva <edsilvasp@hotmail.com>
2019-01-19 10:22:04 -0200

    update visit with Jpa entity mappings closes #37

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -4,6 +4,8 @@ import guru.springframework.sfgpetclinic.services.BaseEntity;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "pets")
@@ -20,6 +22,9 @@ public class Pet extends BaseEntity {
 
     @Column(name = "birth_date")
     private LocalDate birthDate;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
 
 
     public String getName() {
